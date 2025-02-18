bc73ad4fd732738fdbeb409e82f8b6391661793a
Your Name <you@example.com>
2025-02-18 15:47:27 +0900

    BOJ-EX: 2/18/2025, 3:47:25 PM

--- 1749번： 점수따먹기/점수따먹기.cpp
+++ 1749번： 점수따먹기/점수따먹기.cpp
@@ -6,6 +6,7 @@ int main()
     int N,M;
     int board[222][222]={0,};
     int sumboard[222][222]={0,};
+    int ans=-987654321;
 
     cin>>N>>M;
     for(int i=0;i<N;i++)
@@ -31,8 +32,36 @@ int main()
                 sumboard[i][j]+=sumboard[i-1][j]+sumboard[i][j-1]-sumboard[i-1][j-1];
         }
     }
-    for(int i=0;i<N;i++)
-        for(int j=0;j<M;j++)
-            cout<<sumboard[i][j]<<" ";
-        cout<<"\n";
+    // for(int i=0;i<N;i++)
+    // {
+    //     for(int j=0;j<M;j++)
+    //         cout<<sumboard[i][j]<<" ";
+    //     cout<<"\n";
+    // }
+    ans=sumboard[0][0];
+    for(int y=0;y<N;y++)
+    {
+        for(int x=0;x<M;x++)
+        {
+            for(int i=0;i<=y;i++)
+            {
+                for(int j=0;j<=x;j++)
+                {
+                    int a;
+                    if(i==0&&j==0)
+                        continue;
+                    else if(i==0)
+                        a=sumboard[y][x]-sumboard[y][x-j];
+                    else if(j==0)
+                        a=sumboard[y][x]-sumboard[y-i][x];
+                    else
+                        a=sumboard[y][x]-sumboard[y-i][x]-sumboard[y][x-j]+sumboard[y-i][x-j];
+                    ans=max(ans,a);
+                    ans=max(ans,sumboard[y][x]);
+                }
+            }
+        }
+    
+    }
+    cout<<ans;
 }
