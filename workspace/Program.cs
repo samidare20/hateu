798a21c0c73a3712492413d2818c43ae7fd71e6b
samidare20 <dolphine0019@gmail.com>
2026-02-25 11:56:00 +0900

    dd

--- workspace/Program.cs
+++ workspace/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+class Program
+{
+    [Conditional("DEBUG")]
+    static void DebugPrint(object o)
+    {
+        Console.Write(o.ToString());
+    }
+    [Conditional("DEBUG")]
+    static void DebugPrintLine(object o)
+    {
+        if (o == null)
+            o = "";
+        Console.WriteLine(o.ToString());
+    }
+    static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+    static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+    public static void Main()
+    {
+        sr.ReadLine();
+        char[] tmp = sr.ReadLine().ToCharArray();
+        Dictionary<char,int>d= new Dictionary<char,int>();
+        d['A'] = 0;
+        d['B'] = 0;
+        foreach(char i in tmp)
+        {
+            d[i] += 1;
+        }
+        if (d['A'] > d['B'])
+            sw.Write("A");
+        else if (d['A'] < d['B'])
+            sw.Write("B");
+        else
+            sw.Write("Tie");
+        sw.Close();
+    }
+}
